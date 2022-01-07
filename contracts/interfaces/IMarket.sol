7f0ee776f3dc2a159ac12a5ba206a2fda062ba66
Tony <tony@example.com>
2022-01-07 14:05:25 -0600

    Allow flash swapping during liquidation

--- contracts/interfaces/IMarket.sol
+++ contracts/interfaces/IMarket.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import { IFlashSwap } from "./IFlashSwap.sol";
+
 interface IMarket {
     function deposit(address _to, uint _amount) external;
     function withdraw(address _to, uint _amount) external;
@@ -10,7 +12,7 @@ interface IMarket {
     function depositAndBorrow(uint _collateralAmount, uint _debtAmount) external;
     function repayAndWithdraw(uint _debtAmount, uint _collateralAmount) external;
 
-    function liquidate(address _user, uint _amount, address _to) external;
+    function liquidate(address _user, uint _amount, address _to, IFlashSwap swapper) external;
     function updatePrice() external returns (uint);
 
     function getUserLTV(address _user) external view returns(uint);
