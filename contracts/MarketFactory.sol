7cce3c5b8c9a90d03d48d722c73d26fd402b5de2
Brent Fitzgerald <burnto@gmail.com>
2022-01-08 10:08:27 -0800

    Set owner for MarketFactory and created markets

--- contracts/MarketFactory.sol
+++ contracts/MarketFactory.sol
@@ -10,17 +10,35 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
  * Factory for Markets
  **/
 contract MarketFactory is Ownable {
+    /**
+     * @notice New market created
+     * @param index Index of the current market in `markets` list.
+     */
     event CreateMarket(uint256 index);
 
     IMarket[] public markets;
-    ZeroInterestMarket private referenceImpl;
+    ZeroInterestMarket private zeroInterestMarketImpl;
 
+    /**
+     * @notice Create new MarketFactory with an owner
+     * @param _owner Owner of the factory and all markets
+     */
     constructor(address _owner) {
-        referenceImpl = new ZeroInterestMarket();
+        zeroInterestMarketImpl = new ZeroInterestMarket();
         transferOwnership(_owner);
     }
 
-    function createMarket(
+    /**
+     * @notice Create new ZeroInterestMarket owned by this contract's owner
+     * @param _treasury the account that receives fees
+     * @param _collateralToken ERC-20 to be deposited as collateral
+     * @param _debtToken ERC-20 to be withdrawn as debt
+     * @param _oracle Oracle from which to fetch updated collateral/debt token price
+     * @param _maxLoanToValue Maximum ratio of debt to collateral
+     * @param _borrowRate Rate to calculate flat borrow fee
+     * @param _liquidationPenalty Rate to calculate liquidation penalty
+     */
+    function createZeroInterestMarket(
         address _treasury,
         address _collateralToken,
         address _debtToken,
@@ -29,8 +47,9 @@ contract MarketFactory is Ownable {
         uint256 _borrowRate,
         uint256 _liquidationPenalty
     ) public onlyOwner {
-        ZeroInterestMarket market = ZeroInterestMarket(Clones.clone(address(referenceImpl)));
+        ZeroInterestMarket market = ZeroInterestMarket(Clones.clone(address(zeroInterestMarketImpl)));
         market.initialize(
+            owner(),
             _treasury,
             _collateralToken,
             _debtToken,
