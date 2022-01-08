35ec8c8be584f8b654ffb7714093483ca93ecdb6
Brent Fitzgerald <burnto@gmail.com>
2022-01-07 16:19:10 -0800

    add prettier task and check

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    printWidth: 120,
+};
