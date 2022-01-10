fdcfd8a97d77e3d7c6e9b3daf5a66c7cc38dbb5c
bf <flanblob@gmail.com>
2022-01-10 13:45:31 -0800

    remove separate library deployment/linking; embedded use case

--- deploy/02_deploy_mainnet_oracle.ts
+++ deploy/02_deploy_mainnet_oracle.ts
@@ -6,17 +6,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deploy } = hre.deployments;
     const { deployer } = await hre.getNamedAccounts();
 
-    const safeAggregatorV3 = await deploy("SafeAggregatorV3", {
-        from: deployer,
-        log: true,
-    });
-
     await deploy("MainnetgOHMOracle", {
         from: deployer,
         args: [gOHM, OHM_ETH, ETH_USD],
-        libraries: {
-            SafeAggregatorV3: safeAggregatorV3.address,
-        },
         log: true,
     });
 };
