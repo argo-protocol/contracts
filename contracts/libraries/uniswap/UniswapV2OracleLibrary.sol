4c990b129c54c24c393287a33b31870a0371d9de
kushal256 <kushal256@maybe.com>
2022-01-18 17:01:33 -0800

    Found uniswap lib that is friendly with 0.8.x

--- contracts/libraries/uniswap/UniswapV2OracleLibrary.sol
+++ contracts/libraries/uniswap/UniswapV2OracleLibrary.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import { FixedPoint } from "@uniswap/lib/contracts/libraries/FixedPoint.sol";
 
+
 // library with helper methods for oracles that are concerned with computing average prices
 library UniswapV2OracleLibrary {
     using FixedPoint for *;
