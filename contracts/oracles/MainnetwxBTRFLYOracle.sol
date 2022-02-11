1bd743ce6808a3e65cc1b0e22da96772926b4987
kushal256 <3527146+kushal256@users.noreply.github.com>
2022-02-11 14:07:51 -0800

    Redacted oracle (#23)
    * WIP:  Currently failing on depedency of old uniswap libs that do not compile with 0.8.x
    
    * wip
    
    * wip
    
    * Found uniswap lib that is friendly with 0.8.x
    
    * wip
    
    * test github persist-credentials
    
    * testing CI
    
    * testing CI
    
    * testing CI
    
    * wip
    
    * wip
    
    * yarn add https://github.com/artman325/uniswap-lib#e28aa48a641044e3903fef81bde75640f3d944e1 -D
    
    * wip
    
    * yarn add https://github.com/artman325/uniswap-lib#e28aa48a641044e3903fef81bde75640f3d944e1 -D
    
    * yarn add https://github.com/artman325/uniswap-lib#e28aa48a641044e3903fef81bde75640f3d944e1 -D
    
    * Add deployment
    
    * CI test
    
    * CI test
    
    * CI test
    
    * CI test
    
    * Fix linter
    
    * wip
    
    Co-authored-by: kushal256 <kushal256@maybe.com>

---
+++ contracts/oracles/MainnetwxBTRFLYOracle.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IOracle } from "../interfaces/IOracle.sol";
+import { AggregatorV3Interface } from "../interfaces/AggregatorV3Interface.sol";
+import { SafeAggregatorV3 } from "../libraries/SafeAggregatorV3.sol";
+import { UniswapPairOracle } from "./UniswapPairOracle.sol";
+import "hardhat/console.sol";
+
+interface IwxBTRFLY {
+    /**
+        @notice converts wBTRFLY amount to sBTRFLY
+        @dev see https://github.com/redacted-cartel/REDACTED-Smart-Contracts/blob/058416542dbb2a2a532a54328ce2df0f10a34bb0/contracts/WXBTRFLY.sol#L912
+        @param _amount uint
+        @return uint
+     */
+    function xBTRFLYValue(uint256 _amount) external view returns (uint256);
+
+    /**
+        @notice converts sBTRFLY amount to wBTRFLY
+        @dev see https://github.com/redacted-cartel/REDACTED-Smart-Contracts/blob/058416542dbb2a2a532a54328ce2df0f10a34bb0/contracts/WXBTRFLY.sol#L921
+        @param _amount uint
+        @return uint
+     */
+    function wBTRFLYValue(uint256 _amount) external view returns (uint256);
+}
+
+/**
+ * @notice price oracle for wxBTRFLY-USD on the ethereum mainnet
+ * @dev see https://github.com/argo-protocol/contracts/issues/19
+ */
+contract MainnetwxBTRFLYOracle is IOracle {
+    using SafeAggregatorV3 for AggregatorV3Interface;
+
+    IwxBTRFLY private wxBTRFLY;
+    address private btrfly;
+    UniswapPairOracle private uniswapPairOracle;
+    AggregatorV3Interface private ohmEthFeed;
+    AggregatorV3Interface private ethUsdFeed;
+
+    constructor(
+        address _wxBTRFLY,
+        address _btrfly,
+        address _uniswapPairOracle, // sushiswap oracle
+        address _ohmEthFeed,
+        address _ethUsdFeed
+    ) {
+        require(_wxBTRFLY != address(0), "Oracle: 0x0 wxBTRFLY address");
+        require(_btrfly != address(0), "Oracle: 0x0 BTRFLY address");
+        require(_uniswapPairOracle != address(0), "Oracle: 0x0 pairOracle address");
+        require(_ohmEthFeed != address(0), "Oracle: 0x0 OHM-ETH address");
+        require(_ethUsdFeed != address(0), "Oracle: 0x0 ETH-USD address");
+
+        wxBTRFLY = IwxBTRFLY(_wxBTRFLY);
+        btrfly = _btrfly;
+        ohmEthFeed = AggregatorV3Interface(_ohmEthFeed);
+        ethUsdFeed = AggregatorV3Interface(_ethUsdFeed);
+        uniswapPairOracle = UniswapPairOracle(_uniswapPairOracle);
+    }
+
+    function update() external {
+        if (uniswapPairOracle.canUpdate()) {
+            uniswapPairOracle.update();
+        }
+    }
+
+    /**
+     * @notice fetches the latest price
+     * @dev wxBTRFLY/USD = IwxBTRFLY::wBTRFLYValue(BTRFLY/OHM * OHM/ETH * ETH/USD)
+     * @return the price with 18 decimals
+     */
+    function fetchPrice() external view override returns (bool, uint256) {
+        (bool ethUsdSuccess, uint256 ethUsdPrice) = ethUsdFeed.safeLatestRoundData();
+        (bool ohmEthSuccess, uint256 ohmEthPrice) = ohmEthFeed.safeLatestRoundData();
+
+        uint256 btrflyOhmPrice = uniswapPairOracle.consult(btrfly, 1e9);
+        console.log("consult btrflyOhmPrice:", btrflyOhmPrice); //10
+
+        if (!ethUsdSuccess || !ohmEthSuccess) {
+            return (false, 0);
+        }
+
+        console.log("ohmEthPrice:", ohmEthPrice);
+        console.log("ethUsdPrice:", ethUsdPrice);
+        uint256 ohmUsdPrice = (ohmEthPrice * ethUsdPrice) / 1e36; //386
+        console.log("ohmUsdPrice:", ohmUsdPrice);
+
+        uint256 btrflyUsdPrice = (btrflyOhmPrice * ohmEthPrice * ethUsdPrice) / 1e36; //1e18 for ohmEthPrice, 1e18 for ethUsdPrice
+        //1e9 for btrflyOhm
+
+        console.log("btrflyUsdPrice:", btrflyUsdPrice);
+        return (true, wxBTRFLY.wBTRFLYValue(btrflyUsdPrice));
+    }
+}
--- deploy.config.ts
+++ deploy.config.ts
@@ -23,6 +23,11 @@ export default  {
         ethUsdAggregator: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
         psmBuyFee: "250",
         psmSellFee: "250",
+        wxBtrfly: "0x186E55C0BebD2f69348d94C4A27556d93C5Bd36C",
+        btrfly: "0xc0d4ceb216b3ba9c3701b291766fdcba977cec3a",
+        sushiSwapFactory: "0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac",        
+        ohmv2: "0x64aa3364f17a4d01c6f1751fd97c2bd3d7e7f1d5",    
+        uniswapOracle: ZERO_ADDRESS,  // Created in 05_mainnet_wxBTRFLY_oracle.ts
     },
     rinkeby: {
         treasuryMultisig: "0xfE07A76856A6FFD96ddF466DEdedab1d76355A6b",
@@ -33,6 +38,11 @@ export default  {
         ethUsdAggregator: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
         psmBuyFee: "250",
         psmSellFee: "250",
+        wxBtrfly: "0xB4Aaf6857411248A79B95bcb1C13E86140fE9C29", // Not on rinkeby, just pointing to gohm
+        btrfly: "0xB4Aaf6857411248A79B95bcb1C13E86140fE9C29", // Not on rinkeby, just pointing to gohm
+        sushiSwapFactory: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+        ohmv2: "0x10b27a31AA4d7544F89898ccAf3Faf776F5671C4",   
+        uniswapOracle: ZERO_ADDRESS, //Created in 05_mainnet_wxBTRFLY_oracle.ts
     },
     hardhat: {
         treasuryMultisig: "0xfE07A76856A6FFD96ddF466DEdedab1d76355A6b",
@@ -45,5 +55,10 @@ export default  {
         ethUsdAggregator: ZERO_ADDRESS,
         psmBuyFee: "250",
         psmSellFee: "250",
+        wxBtrfly: ZERO_ADDRESS,
+        btrfly: ZERO_ADDRESS,
+        sushiSwapFactory: ZERO_ADDRESS,        
+        ohmv2: ZERO_ADDRESS,   
+        uniswapOracle: ZERO_ADDRESS,
     }
 } as { [name: string]: ChainConfig };
