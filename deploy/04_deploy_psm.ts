c00abb88e96e2ec83a6713bd06fd1de404b2d6d7
Tony <tony@example.com>
2022-02-27 20:48:44 -0600

    Fix up some deploy scripts

--- deploy/04_deploy_psm.ts
+++ deploy/04_deploy_psm.ts
@@ -2,10 +2,11 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import configs from "../deploy.config";
 import { assert } from "console";
+import { deployments } from "hardhat";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deploy } = hre.deployments;
-    const { deployer } = await hre.getNamedAccounts();
+    const { deployer, operatorMultisig } = await hre.getNamedAccounts();
     const config = configs[hre.network.name];
     const debtToken = await hre.deployments.get("DebtToken");
 
@@ -15,12 +16,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         config.dai = daiDeployment.address;
     }
 
-    await deploy("DaiPSM", {
+    let daiPSMResult = await deploy("DaiPSM", {
         contract: "PegStability",
         from: deployer,
         args: [debtToken.address, config.dai, config.psmBuyFee, config.psmSellFee, config.treasuryMultisig],
         log: true,
     });
+    const DaiPSMContract = await hre.ethers.getContractFactory("PegStability");
+    const contract = await DaiPSMContract.attach(daiPSMResult.address);
+    if (await contract.owner() != operatorMultisig) {
+        deployments.log("transfering ownership to operatorMultisig");
+        await contract.transferOwnership(operatorMultisig);
+    }
 };
 func.dependencies = ["StubDai", "DebtToken"];
 func.tags = ["PegStability"];
