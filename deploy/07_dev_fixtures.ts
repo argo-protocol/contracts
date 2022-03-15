3f1b544caafecd8fae73231e82202301ba1444fa
bf <flanblob@gmail.com>
2022-03-14 22:35:08 -0700

    update fixtures to use oracle price of 1.5

--- deploy/07_dev_fixtures.ts
+++ deploy/07_dev_fixtures.ts
@@ -17,25 +17,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     const oracleDeployment = await hre.deployments.get("gOhmOracle");
     const oracle = await hre.ethers.getContractAt("StubOracle", oracleDeployment.address);
-    await oracle.setPrice(hre.ethers.utils.parseEther("1"));
-    hre.deployments.log("StubOracle price set to 1");
+    await oracle.setPrice(hre.ethers.utils.parseEther("1.5"));
+    hre.deployments.log("StubOracle price set to 1.5");
 
     const debtTokenDeployment = await hre.deployments.get("DebtToken");
     const debtToken = await hre.ethers.getContractAt("DebtToken", debtTokenDeployment.address);
 
     const deployment = await hre.deployments.get("MarketFactory");
     const marketFactory = await hre.ethers.getContractAt("MarketFactory", deployment.address);
-    const tx = await marketFactory
-        .connect(signer)
-        .createZeroInterestMarket(
-            treasuryMultisig,
-            stubDai.address,
-            debtTokenDeployment.address,
-            oracleDeployment.address,
-            "60000",
-            "1000",
-            "10000"
-        );
+    const tx = await marketFactory.connect(signer).createZeroInterestMarket(
+        treasuryMultisig,
+        stubDai.address,
+        debtTokenDeployment.address,
+        oracleDeployment.address,
+        "60000", // 60%
+        "1000", // 1%
+        "10000" // 10%
+    );
     await tx.wait();
     const logs = await marketFactory.queryFilter(marketFactory.filters.CreateMarket(), "latest");
     const id = logs[0].args[0];
