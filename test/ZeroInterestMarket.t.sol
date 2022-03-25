d8c7bd72f2331b1ecccab5dd116a583ce914f2d0
bf <flanblob@gmail.com>
2022-03-25 13:49:37 -0700

    add arg to DebtToken constructor in dapptools test

--- test/ZeroInterestMarket.t.sol
+++ test/ZeroInterestMarket.t.sol
@@ -9,7 +9,6 @@ import { StubOracle } from "../contracts/stubs/StubOracle.sol";
 import { ERC20Mock } from "../contracts/testing/ERC20Mock.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 
-
 contract ZeroInterestMarketTest is DSTest {
     TestAccount public owner;
     TestAccount public treasury;
@@ -18,14 +17,14 @@ contract ZeroInterestMarketTest is DSTest {
     ERC20Mock public collateralToken;
     DebtToken public debtToken;
     StubOracle public oracle;
-    uint public constant MAX_LTV = 50000;
-    uint public constant BORROW_FEE = 1500;
+    uint256 public constant MAX_LTV = 50000;
+    uint256 public constant BORROW_FEE = 1500;
 
     function setUp() public {
         owner = new TestAccount("owner", address(0), address(0));
         treasury = new TestAccount("treasury", address(0), address(0));
         collateralToken = new ERC20Mock("TEST", "Test Token", address(this), 1e18);
-        debtToken = new DebtToken(address(treasury));
+        debtToken = new DebtToken(address(treasury), address(0x0));
         oracle = new StubOracle();
         market = new ZeroInterestMarket();
         market.initialize(
@@ -45,17 +44,17 @@ contract ZeroInterestMarketTest is DSTest {
     //// FUZZ TESTS
     /////////
 
-    function testBorrowMaintainsLTV(uint _price, uint _borrowAmount) public {
+    function testBorrowMaintainsLTV(uint256 _price, uint256 _borrowAmount) public {
         if (_borrowAmount > 1e40) return;
         if (_price > 1e40) return;
         if (_price < 1e5) return;
 
-        uint debtAmount = _borrowAmount + (_borrowAmount * BORROW_FEE / market.BORROW_RATE_PRECISION());
+        uint256 debtAmount = _borrowAmount + ((_borrowAmount * BORROW_FEE) / market.BORROW_RATE_PRECISION());
         debtToken.mint(address(market), debtAmount);
         oracle.setPrice(_price);
         collateralToken.approve(address(market), 1e18);
 
-        uint ltv = debtAmount * market.LOAN_TO_VALUE_PRECISION() / _price;
+        uint256 ltv = (debtAmount * market.LOAN_TO_VALUE_PRECISION()) / _price;
 
         if (ltv <= MAX_LTV) {
             market.depositAndBorrow(1e18, _borrowAmount);
@@ -71,14 +70,14 @@ contract ZeroInterestMarketTest is DSTest {
         }
     }
 
-    function testLiquidateAlwaysProfitableForLiquidator(uint _newPrice, uint _repayAmount) public {
+    function testLiquidateAlwaysProfitableForLiquidator(uint256 _newPrice, uint256 _repayAmount) public {
         if (_newPrice > 1e60) return;
         if (_newPrice > 1e5) return;
         if (_repayAmount > 1e60) return;
         if (_repayAmount < 1e5) return;
 
-        uint initialPrice = 100000e18; // $100,000
-        uint borrowAmount = 45000e18;
+        uint256 initialPrice = 100000e18; // $100,000
+        uint256 borrowAmount = 45000e18;
 
         debtToken.mint(address(liquidator), _repayAmount);
         debtToken.mint(address(market), 60000e18);
@@ -91,8 +90,9 @@ contract ZeroInterestMarketTest is DSTest {
         if (_newPrice < 90000e18) {
             // this will cause a liquidation
             liquidator.liquidate(address(this), _repayAmount);
-            uint valueOfLiqAssets = collateralToken.balanceOf(address(liquidator)) * _newPrice / market.LAST_PRICE_PRECISION();
-            uint amountPaid = _repayAmount - debtToken.balanceOf(address(liquidator));
+            uint256 valueOfLiqAssets = (collateralToken.balanceOf(address(liquidator)) * _newPrice) /
+                market.LAST_PRICE_PRECISION();
+            uint256 amountPaid = _repayAmount - debtToken.balanceOf(address(liquidator));
             assertGt(valueOfLiqAssets, amountPaid);
         } else {
             // revert for not liquidating
@@ -104,4 +104,3 @@ contract ZeroInterestMarketTest is DSTest {
         }
     }
 }
-
