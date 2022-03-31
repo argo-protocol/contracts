9e352e3e5d6c4b154d7ce7e77ac024e4725115db
bf <flanblob@gmail.com>
2022-03-31 12:37:05 -0700

    adjust language in LayerZeroController and add tests

--- contracts/LayerZeroController.sol
+++ contracts/LayerZeroController.sol
@@ -20,14 +20,16 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ILayerZeroReceiver } from "./interfaces/LayerZero/ILayerZeroReceiver.sol";
 import { ILayerZeroEndpoint } from "./interfaces/LayerZero/ILayerZeroEndpoint.sol";
 import { IDebtToken } from "./interfaces/IDebtToken.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @notice A collateralized debt position token. Protocol assumes this is worth $1.
  */
 contract LayerZeroController is ILayerZeroReceiver, Ownable {
-    mapping(uint16 => bytes) public lzRemotes;
-    ILayerZeroEndpoint public lzEndpoint;
-    address public lzAdmin;
+    using SafeERC20 for IERC20;
+
+    mapping(uint16 => bytes) public remotes;
+    ILayerZeroEndpoint public endpoint;
     IDebtToken public token;
 
     constructor(IDebtToken _token) {
@@ -45,12 +47,13 @@ contract LayerZeroController is ILayerZeroReceiver, Ownable {
         bytes calldata _dstOmniChainTokenAddr,
         uint256 _qty
     ) public payable requireLayerZeroEnabled {
-        token.transferFrom(msg.sender, address(this), _qty);
+        require(token.allowance(msg.sender, address(this)) >= _qty, "LZC: low allowance");
+        require(token.transferFrom(msg.sender, address(this), _qty), "LZC: transfer fail");
         token.burn(_qty);
         bytes memory payload = abi.encode(msg.sender, _qty);
 
         /* solhint-disable check-send-result */
-        lzEndpoint.send{ value: msg.value }(
+        endpoint.send{ value: msg.value }(
             _chainId,
             _dstOmniChainTokenAddr,
             payload,
@@ -73,11 +76,11 @@ contract LayerZeroController is ILayerZeroReceiver, Ownable {
         uint64,
         bytes memory _payload
     ) external override requireLayerZeroEnabled {
-        require(msg.sender == address(lzEndpoint), "DebtToken: lzReceive bad sender");
+        require(msg.sender == address(endpoint), "LZC: lzReceive bad sender");
         require(
-            _srcAddress.length == lzRemotes[_srcChainId].length &&
-                keccak256(_srcAddress) == keccak256(lzRemotes[_srcChainId]),
-            "DebtToken: lzReceive bad remote"
+            _srcAddress.length == remotes[_srcChainId].length &&
+                keccak256(_srcAddress) == keccak256(remotes[_srcChainId]),
+            "LZC: lzReceive bad remote"
         );
         (address toAddr, uint256 qty) = abi.decode(_payload, (address, uint256));
         token.mint(toAddr, qty);
@@ -89,23 +92,34 @@ contract LayerZeroController is ILayerZeroReceiver, Ownable {
      * @param _remoteAddress The contract address on the remote chainId
      */
     function setLayerZeroRemote(uint16 _chainId, bytes calldata _remoteAddress) external onlyOwner {
-        lzRemotes[_chainId] = _remoteAddress;
+        remotes[_chainId] = _remoteAddress;
     }
 
     /**
      * @notice The owner must set remote contract addresses
-     * @dev Only settable by the lzAdmin address. Disable LayerZero by setting to 0x0.
+     * @dev Only settable by the owner. Disable LayerZero by setting to 0x0.
      * @param _endpoint The address of the ILayerZeroEndpoint contract
      */
     function setLayerZeroEndpoint(address _endpoint) public onlyOwner {
-        lzEndpoint = ILayerZeroEndpoint(_endpoint);
+        endpoint = ILayerZeroEndpoint(_endpoint);
     }
 
     /**
-     * @notice Only allow when lzEndpoint is set
+     * @notice Only allow when endpoint is set
      */
     modifier requireLayerZeroEnabled() {
-        require(address(lzEndpoint) != address(0), "DebtToken: LZ disabled");
+        require(address(endpoint) != address(0), "LZC: disabled");
         _;
+    }
+
+    /**
+     * @notice recover tokens inadvertantly sent to this contract by transfering them to the owner
+     * @param _token the address of the token
+     * @param _amount the amount to transfer
+     */
+    function recoverERC20(address _token, uint256 _amount) external onlyOwner {
+        require(_token != address(token), "LZC: Cannot recover LZC token");
+
+        IERC20(_token).safeTransfer(msg.sender, _amount);
     }
 }
